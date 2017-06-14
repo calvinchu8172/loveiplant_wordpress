a8d0c92c717c98905828320b2d01fc9fe7bc5e99
Calvin Chu <calvinchu@ecoworkinc.com>
2017-06-15 01:09:07 +0800

    Add confirm password field. Reduce woocommerce password strength.

--- wp-content/plugins/theme-customisations-master/custom/functions.php
+++ wp-content/plugins/theme-customisations-master/custom/functions.php
@@ -15,3 +15,39 @@ if ( ! defined( 'ABSPATH' ) ) {
  * functions.php
  * Add PHP snippets here
  */
+
+// Add the code below to your theme's functions.php file to add a confirm password field on the register form under My Accounts.
+add_filter('woocommerce_registration_errors', 'registration_errors_validation', 10,3);
+function registration_errors_validation($reg_errors, $sanitized_user_login, $user_email) {
+  global $woocommerce;
+  extract( $_POST );
+  if ( strcmp( $password, $password2 ) !== 0 ) {
+    return new WP_Error( 'registration-error', __( 'Passwords do not match.', 'woocommerce' ) );
+  }
+  return $reg_errors;
+}
+add_action( 'woocommerce_register_form', 'wc_register_form_password_repeat' );
+function wc_register_form_password_repeat() {
+  ?>
+  <p class="form-row form-row-wide">
+    <label for="reg_password2"><?php _e( '確認密碼', 'woocommerce' ); ?> <span class="required">*</span></label>
+    <input type="password" class="input-text" name="password2" id="reg_password2" value="<?php if ( ! empty( $_POST['password2'] ) ) echo esc_attr( $_POST['password2'] ); ?>" />
+  </p>
+  <?php
+}
+?>
+
+<?php
+/**
+ *Reduce the strength requirement on the woocommerce password.
+ *
+ * Strength Settings
+ * 3 = Strong (default)
+ * 2 = Medium
+ * 1 = Weak
+ * 0 = Very Weak / Anything
+ */
+function reduce_woocommerce_min_strength_requirement( $strength ) {
+    return 1;
+}
+add_filter( 'woocommerce_min_password_strength', 'reduce_woocommerce_min_strength_requirement' );
